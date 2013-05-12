1e1cc76b6e59179f9c5cadd8f9412430d740e426
Raekye <withoutpointk@gmail.com>
2013-05-12 00:24:46 -0700

    small code fixes - added error stream to process redirector so logging works in spawned processes - close on launch fix

--- src/com/mineshaftersquared/gui/tabs/IndexTabPane.java
+++ src/com/mineshaftersquared/gui/tabs/IndexTabPane.java
@@ -107,7 +107,7 @@ public class IndexTabPane extends AbstractTabPane {
 										IndexTabPane.this.prefs
 										.getString("proxy.authserver", UniversalLauncher.DEFAULT_AUTH_SERVER) });
 					new Thread(new ProcessOutputRedirector(p, "[MS2-Game]: ")).start();
-					if (IndexTabPane.this.prefs.getInt("launcher.closeonlaunch", 0) == 1) {
+					if (IndexTabPane.this.prefs.getInt("launcher.closeonlaunch", 1) == 1) {
 						((JFrame) IndexTabPane.this.prefs.tmpGetObject("launcher.window")).dispose();
 					}
 				} catch (IOException ex) {
--- src/com/mineshaftersquared/resources/ProcessOutputRedirector.java
+++ src/com/mineshaftersquared/resources/ProcessOutputRedirector.java
@@ -5,6 +5,7 @@
 package com.mineshaftersquared.resources;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import com.mineshaftersquared.UniversalLauncher;
@@ -24,19 +25,31 @@ public class ProcessOutputRedirector implements Runnable {
 
 	@Override
 	public void run() {
-		BufferedReader stdout = new BufferedReader(new InputStreamReader(this.process.getInputStream()));
-		while (true) {
-			try {
-				String str = stdout.readLine();
-				if (str == null) {
-					throw new NullPointerException("reading stdout was null (is okay)");
+		new Thread(new InputStreamRedirector(this.process.getInputStream())).start();
+		new Thread(new InputStreamRedirector(this.process.getErrorStream())).start();
+		
+	}
+
+	private class InputStreamRedirector implements Runnable {
+		private final BufferedReader out;
+		public InputStreamRedirector(InputStream in) {
+			this.out = new BufferedReader(new InputStreamReader(in));
+		}
+		
+		@Override
+		public void run() {
+			while (true) {
+				try {
+					String str = this.out.readLine();
+					if (str == null) {
+						throw new NullPointerException("reading stdout was null (is okay)");
+					}
+					UniversalLauncher.log.info(ProcessOutputRedirector.this.log + " - " + str);
+				} catch (Exception ex) {
+					UniversalLauncher.log.info("{ProcessOutputRedirector: caught exception {" + ex.getMessage() + "}, is done}");
+					break;
 				}
-				UniversalLauncher.log.info(this.log + " - " + str);
-			} catch (Exception ex) {
-				UniversalLauncher.log.info("{ProcessOutputRedirector: caught exception {" + ex.getMessage() + "}, is done}");
-				break;
 			}
 		}
 	}
-
 }
