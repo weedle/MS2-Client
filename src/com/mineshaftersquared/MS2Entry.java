45ba463b4b9b62d33c96d2b9c6e210c976be97b8
Raekye <adrian@creatifcubed.com>
2013-07-24 16:57:57 -0700

    update entry

--- src/com/mineshaftersquared/MS2Entry.java
+++ src/com/mineshaftersquared/MS2Entry.java
@@ -16,6 +16,8 @@ public class MS2Entry {
 	public static final Option bukkitOption = new Option("bukkit", false, "server: Use this flag for Bukkit and derivatives");
 	public static final Option authserverOption = new Option("authserver", true, "server: Domain to poll for authentication, skins, etc. Defaults to " + UniversalLauncher.DEFAULT_AUTH_SERVER);
 	public static final Option mcSeparatorOption = new Option("mc", false, "Everything after this is an argument for Minecraft");
+	public static final Option updateOption = new Option("update", false, "Update the launcher");
+	public static final Option argOption = new Option("arg", true, "[Dev use]");
 	
 	public static final Options cmdOptions = new Options() {{
 		addOption("help", false, "About the Mineshafter Squared Universal Launcher");
@@ -23,9 +25,10 @@ public class MS2Entry {
 		addOption(serverOption);
 		addOption(bukkitOption);
 		addOption(authserverOption);
-		addOption("update", false, "Update the launcher");
+		addOption(updateOption);
 		addOption("game", false, "Start the Minecraft launcher directly");
 		addOption("gui", false, "Start the GUI (ignores " + UniversalLauncher.MC_START_AUTOMATICALLY + ")");
+		addOption(argOption);
 		addOption(mcSeparatorOption);
 	}};
 	
@@ -45,12 +48,10 @@ public class MS2Entry {
 			MCEntry.main(args);
 		} else if (cmd.hasOption("help")) {
 			showHelp();
+		} else if (cmd.hasOption("update")) {
+			MS2LauncherUpdate.main(args);
 		} else if (args.length > 0) {
-			if (args[0].startsWith("update")) {
-				MS2LauncherUpdate.main(args);
-			} else {
-				showHelp();
-			}
+			showHelp();
 		} else {
 			if (new File(UniversalLauncher.MC_START_AUTOMATICALLY).exists()) {
 				// TODO: check updates
--- src/com/mineshaftersquared/MS2LauncherUpdate.java
+++ src/com/mineshaftersquared/MS2LauncherUpdate.java
@@ -1,11 +1,104 @@
 package com.mineshaftersquared;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.io.FileUtils;
+
+import com.creatifcubed.simpleapi.SimpleUtils;
+import com.mineshaftersquared.misc.ExtendedGnuParser;
+
 public class MS2LauncherUpdate {
+	
+	public static final Options options = new Options();
+	public static final String JAR_NAME = "mineshaftersquared.jar";
+	public static final String NEW_JAR_NAME = "mineshaftersquared-new.jar";
+	public static final String OLD_JAR_RENAME = "mineshaftersquared-old.jar";
+	
+	static {
+		options.addOption(MS2Entry.updateOption);
+		options.addOption(MS2Entry.argOption);
+	}
 	
 	/**
 	 * @param args
+	 * @throws ParseException 
+	 * @throws MalformedURLException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException, IOException {
+		CommandLineParser cmdParser = new ExtendedGnuParser();
+		CommandLine cmd = cmdParser.parse(options, args);
 		
+		String stepStr = cmd.getOptionValue("update");
+		int step = 0;
+		if (stepStr != null) {
+			try {
+				step = Integer.parseInt(stepStr);
+			} catch (NumberFormatException ex) {
+				ex.printStackTrace();
+				return;
+			}
+		}
+		String arg = cmd.getOptionValue("arg");
+		
+		switch (step) {
+		case 0: // old: download new jar
+			step0();
+			break;
+		case 1: // new: rename old jar
+			if (arg == null) {
+				UniversalLauncher.log.info("Must give arg for update (old jar name)");
+				System.exit(1);
+			}
+			step1(arg);
+			break;
+		case 2: // old: rename new jar, delete self
+			if (arg == null) {
+				UniversalLauncher.log.info("Must give arg for update (new jar name)");
+				System.exit(1);
+			}
+			step2(arg);
+			break;
+		}
+	}
+	
+	public static void step0() throws IOException {
+		File self = SimpleUtils.getJarPath(UniversalLauncher.class);
+		File update = new File(NEW_JAR_NAME);
+		
+		FileUtils.copyURLToFile(new URL(UniversalLauncher.POLLING_SERVER + "latestdownload.php?jar=yes"), update);
+		
+		ProcessBuilder pb = new ProcessBuilder("java", "-jar", update.getCanonicalPath(), "-update=1", "-arg=" + self.getName());
+		pb.start();
+	}
+	
+	public static void step1(String oldJar) throws IOException {
+		File self = SimpleUtils.getJarPath(UniversalLauncher.class);
+		File oldFile = new File(oldJar);
+		File oldRename = new File(OLD_JAR_RENAME);
+		
+		oldFile.renameTo(oldRename);
+		
+		ProcessBuilder pb = new ProcessBuilder("java", "-jar", oldRename.getCanonicalPath(), "-update=2", "-arg=" + self.getName());
+		pb.start();
+	}
+	
+	public static void step2(String newJar) throws IOException {
+		File self = SimpleUtils.getJarPath(UniversalLauncher.class);
+		File newFile = new File(newJar);
+		File newRename = new File(JAR_NAME);
+		
+		newFile.renameTo(newRename);
+		
+		ProcessBuilder pb = new ProcessBuilder("java", "-jar", newRename.getCanonicalPath());
+		pb.start();
+		
+		self.deleteOnExit();
 	}
 }
