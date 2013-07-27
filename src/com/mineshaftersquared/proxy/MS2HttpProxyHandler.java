fbc62befb1d534fd7bca231c28afa7052ea40d9c
Ryan Sullivan <kayoticsully@gmail.com>
2013-07-27 19:41:17 -0400

    Added stubs for joinserver and check server

--- src/com/mineshaftersquared/proxy/MS2HttpProxyHandler.java
+++ src/com/mineshaftersquared/proxy/MS2HttpProxyHandler.java
@@ -26,151 +26,166 @@ import com.mineshaftersquared.misc.MS2Utils;
  * Modified by Adrian
  */
 public class MS2HttpProxyHandler implements MS2Proxy.Handler {
-	
-	private final Map<String, byte[]> skinCache;
-	private final Map<String, byte[]> cloakCache;
-	
-	private final MCYggdrasilOffline yggdrasilOffline;
-	
-	public MS2HttpProxyHandler() {
-		this.skinCache = new HashMap<String, byte[]>();
-		this.cloakCache = new HashMap<String, byte[]>();
-		this.yggdrasilOffline = new MCYggdrasilOffline(new File(MS2Utils.getDefaultMCDir(), "launcher_profiles.json"));
-	}
-	
-	public void handle(MS2Proxy proxy, Socket socket) {
-		try {
-			
-		} finally {
-			IOUtils.closeQuietly(socket);
-		}
-	}
-	
-	public boolean respondsTo(String method) {
-		return Arrays.asList("get", "post", "head", "connect").contains(method);
-	}
-	
-	public boolean on(String method, String url, Map<String, String> headers, InputStream in, OutputStream out, MS2Proxy ms2Proxy) {
-		method = method.toLowerCase();
-		if (method.equals("get")) {
-			return this.onGet(url, headers, in, out, ms2Proxy);
-		} else if (method.equals("post")) {
-			return this.onPost(url, headers, in, out, ms2Proxy);
-		} else if (method.equals("head")) {
-			return this.onHead(url, headers, in, out, ms2Proxy);
-		} else if (method.equals("connect")) {
-			return this.onConnect(url, headers, in, out, ms2Proxy);
-		} else {
-			throw new IllegalArgumentException("Unknown method " + method);
-		}
-	}
 
-	public boolean onGet(String url, Map<String, String> headers, InputStream in, OutputStream out, MS2Proxy ms2Proxy) {
-		UniversalLauncher.log.info("Proxy - get - " + url);
-		Matcher skinMatcher = MS2Proxy.SKIN_URL.matcher(url);
-		Matcher cloakMatcher = MS2Proxy.CLOAK_URL.matcher(url);
-		Matcher checkserverMatcher = MS2Proxy.CHECKSERVER_URL.matcher(url);
-		Matcher joinserverMatcher = MS2Proxy.JOINSERVER_URL.matcher(url);
-		if (skinMatcher.matches()) {
-			String username = skinMatcher.group(1);
-			UniversalLauncher.log.info("Proxy - skin - " + username);
-			byte[] data = this.skinCache.get(username);
-			if (data == null) {
-				data = new SimpleHTTPRequest(ms2Proxy.routes.getSkinURL()).addGet("username", username).doGet();
-			}
-			this.skinCache.put(username, data);
-			this.sendResponse(out, "image/png", data);
-			return true;
-		} else if (cloakMatcher.matches()) {
-			String username = cloakMatcher.group(1);
-			UniversalLauncher.log.info("Proxy - cloak - " + username);
-			byte[] data = this.cloakCache.get(username);
-			if (data == null) {
-				data = new SimpleHTTPRequest(ms2Proxy.routes.getCloakURL()).addGet("username", username).doGet();
-			}
-			this.cloakCache.put(username, data);
-			this.sendResponse(out, "image/png", data);
-			return true;
-		} else if (checkserverMatcher.matches()) {
-			this.sendResponse(out, "text/plain", "yes".getBytes(Charset.forName("utf-8")));
-			return true;
-		} else if (joinserverMatcher.matches()) {
-			this.sendResponse(out, "text/plain", "ok".getBytes(Charset.forName("utf-8")));
-			return true;
-		} else {
-			return false;
-		}
-	}
-	public boolean onPost(String url, Map<String, String> headers, InputStream in, OutputStream out, MS2Proxy ms2Proxy) {
-		UniversalLauncher.log.info("Proxy - post - " + url);
-		int contentLength = Integer.parseInt(headers.get("content-length"));
-		Matcher authServerMatcher = MS2Proxy.AUTH_URL.matcher(url);
-		
-		if (authServerMatcher.matches()) {
-			UniversalLauncher.log.info("Proxy - auth");
-			
-			String action = authServerMatcher.group(1);
-			try {
-				char[] body = new char[contentLength];
-				InputStreamReader reader = new InputStreamReader(in/*, Charset.forName("utf-8")*/);
-				int x = reader.read(body);
-				String postedJSON = new String(body);
-				for (String key : headers.keySet()) {
-					System.out.println(key + ": " + headers.get(key));
-				}
-				System.out.println("Content length was : " + contentLength);
-				System.out.println("Read: " + x);
-				System.out.println("String length: " + postedJSON.length());
-				System.out.println("Posted json: " + postedJSON);
-				System.out.flush();
-				String response = this.authServerAction(action, postedJSON, ms2Proxy);
-				this.sendResponse(out, "application/json", response.getBytes(Charset.forName("utf-8")));
-			} catch (IOException ex) {
-				ex.printStackTrace();
-			}
-			return true;
-		}
-		return false;
-	}
-	public boolean onHead(String url, Map<String, String> headers, InputStream in, OutputStream out, MS2Proxy ms2Proxy) {
-		return false;
-	}
-	public boolean onConnect(String url, Map<String, String> headers, InputStream in, OutputStream out, MS2Proxy ms2Proxy) {
-		return false;
-	}
-	
-	private void sendResponse(OutputStream out, String contentType, byte[] data) {
-		OutputStreamWriter writer = null;
-		try {
-			writer = new OutputStreamWriter(out);
-			writer.append("HTTP/1.0 200 OK\r\nConnection: close\r\nProxy-Connection: close\r\n");
-			writer.append("Content-Length:" + data.length + "\r\n");
-			if (contentType != null) {
-				writer.append("Content-Type: " + contentType + "\r\n\r\n");
-			}
-			writer.flush();
-			out.write(data);
-			out.flush();
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		} finally {
-			IOUtils.closeQuietly(out);
-			IOUtils.closeQuietly(writer);
-		}
-	}
-	
-	private byte[] getRequest(String url) {
-		return new SimpleHTTPRequest(url).doGet(Proxy.NO_PROXY);
-	}
-	
-	private byte[] postRequest(String url, String postdata) {
-		return new SimpleHTTPRequest(url).addPost(postdata).doPost(Proxy.NO_PROXY);
-	}
-	
-	private String authServerAction(String action, String postedJSON, MS2Proxy ms2Proxy) {
-		UniversalLauncher.log.info("Proxy - auth - action: " + action + ", postedJSON - " + postedJSON);
-		Gson gson = new Gson();
-		MCYggdrasilRequest data = gson.fromJson(postedJSON, MCYggdrasilRequest.class);
+    private final Map<String, byte[]> skinCache;
+    private final Map<String, byte[]> cloakCache;
+
+    private final MCYggdrasilOffline yggdrasilOffline;
+
+    public MS2HttpProxyHandler() {
+        this.skinCache = new HashMap<String, byte[]>();
+        this.cloakCache = new HashMap<String, byte[]>();
+        this.yggdrasilOffline = new MCYggdrasilOffline(new File(MS2Utils.getDefaultMCDir(), "launcher_profiles.json"));
+    }
+
+    public void handle(MS2Proxy proxy, Socket socket) {
+        try {
+
+        } finally {
+            IOUtils.closeQuietly(socket);
+        }
+    }
+
+    public boolean respondsTo(String method) {
+        return Arrays.asList("get", "post", "head", "connect").contains(method);
+    }
+
+    public boolean on(String method, String url, Map<String, String> headers, InputStream in, OutputStream out, MS2Proxy ms2Proxy) {
+        method = method.toLowerCase();
+        if (method.equals("get")) {
+            return this.onGet(url, headers, in, out, ms2Proxy);
+        } else if (method.equals("post")) {
+            return this.onPost(url, headers, in, out, ms2Proxy);
+        } else if (method.equals("head")) {
+            return this.onHead(url, headers, in, out, ms2Proxy);
+        } else if (method.equals("connect")) {
+            return this.onConnect(url, headers, in, out, ms2Proxy);
+        } else {
+            throw new IllegalArgumentException("Unknown method " + method);
+        }
+    }
+
+    public boolean onGet(String url, Map<String, String> headers, InputStream in, OutputStream out, MS2Proxy ms2Proxy) {
+        UniversalLauncher.log.info("Proxy - get - " + url);
+
+        Matcher skinMatcher = MS2Proxy.SKIN_URL.matcher(url);
+        Matcher cloakMatcher = MS2Proxy.CLOAK_URL.matcher(url);
+        Matcher checkserverMatcher = MS2Proxy.CHECKSERVER_URL.matcher(url);
+        Matcher joinserverMatcher = MS2Proxy.JOINSERVER_URL.matcher(url);
+
+        if (skinMatcher.matches()) {
+            String username = skinMatcher.group(1);
+            UniversalLauncher.log.info("Proxy - skin - " + username);
+            byte[] data = this.skinCache.get(username);
+            if (data == null) {
+                data = new SimpleHTTPRequest(ms2Proxy.routes.getSkinURL()).addGet("username", username).doGet();
+            }
+            this.skinCache.put(username, data);
+            this.sendResponse(out, "image/png", data);
+            return true;
+        } else if (cloakMatcher.matches()) {
+            String username = cloakMatcher.group(1);
+            UniversalLauncher.log.info("Proxy - cloak - " + username);
+            byte[] data = this.cloakCache.get(username);
+            if (data == null) {
+                data = new SimpleHTTPRequest(ms2Proxy.routes.getCloakURL()).addGet("username", username).doGet();
+            }
+            this.cloakCache.put(username, data);
+            this.sendResponse(out, "image/png", data);
+            return true;
+        } else if (checkserverMatcher.matches()) {
+            UniversalLauncher.log.info("CHECKSERVER");
+            String[] request = url.split("[?]");
+            String response = this.authMultiplayerAction("checkserver", request[1], ms2Proxy);
+
+            this.sendResponse(out, "text/plain", response.getBytes(Charset.forName("utf-8")));
+
+            return true;
+        } else if (joinserverMatcher.matches()) {
+            UniversalLauncher.log.info("JOINSERVER");
+            String[] request = url.split("[?]");
+            String response = this.authMultiplayerAction("joinserver", request[1], ms2Proxy);
+
+            this.sendResponse(out, "text/plain", response.getBytes(Charset.forName("utf-8")));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean onPost(String url, Map<String, String> headers, InputStream in, OutputStream out, MS2Proxy ms2Proxy) {
+        UniversalLauncher.log.info("Proxy - post - " + url);
+        int contentLength = Integer.parseInt(headers.get("content-length"));
+        Matcher authServerMatcher = MS2Proxy.AUTH_URL.matcher(url);
+
+        if (authServerMatcher.matches()) {
+            UniversalLauncher.log.info("Proxy - auth");
+
+            String action = authServerMatcher.group(1);
+            try {
+                char[] body = new char[contentLength];
+                InputStreamReader reader = new InputStreamReader(in/*, Charset.forName("utf-8")*/);
+                int x = reader.read(body);
+                String postedJSON = new String(body);
+                for (String key : headers.keySet()) {
+                    System.out.println(key + ": " + headers.get(key));
+                }
+                System.out.println("Content length was : " + contentLength);
+                System.out.println("Read: " + x);
+                System.out.println("String length: " + postedJSON.length());
+                System.out.println("Posted json: " + postedJSON);
+                System.out.flush();
+
+                String response = this.authServerAction(action, postedJSON, ms2Proxy);
+                this.sendResponse(out, "application/json", response.getBytes(Charset.forName("utf-8")));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public boolean onHead(String url, Map<String, String> headers, InputStream in, OutputStream out, MS2Proxy ms2Proxy) {
+        return false;
+    }
+
+    public boolean onConnect(String url, Map<String, String> headers, InputStream in, OutputStream out, MS2Proxy ms2Proxy) {
+        return false;
+    }
+
+    private void sendResponse(OutputStream out, String contentType, byte[] data) {
+        OutputStreamWriter writer = null;
+        try {
+            writer = new OutputStreamWriter(out);
+            writer.append("HTTP/1.0 200 OK\r\nConnection: close\r\nProxy-Connection: close\r\n");
+            writer.append("Content-Length:" + data.length + "\r\n");
+            if (contentType != null) {
+                writer.append("Content-Type: " + contentType + "\r\n\r\n");
+            }
+            writer.flush();
+            out.write(data);
+            out.flush();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            IOUtils.closeQuietly(out);
+            IOUtils.closeQuietly(writer);
+        }
+    }
+
+    private byte[] getRequest(String url) {
+        return new SimpleHTTPRequest(url).doGet(Proxy.NO_PROXY);
+    }
+
+    private byte[] postRequest(String url, String postdata) {
+        return new SimpleHTTPRequest(url).addPost(postdata).doPost(Proxy.NO_PROXY);
+    }
+
+    private String authServerAction(String action, String postedJSON, MS2Proxy ms2Proxy) {
+        UniversalLauncher.log.info("Proxy - auth - action: " + action + ", postedJSON - " + postedJSON);
+        Gson gson = new Gson();
+        MCYggdrasilRequest data = gson.fromJson(postedJSON, MCYggdrasilRequest.class);
 
         /**
          * The 3 return statements here make the difference between online and offline
@@ -179,26 +194,47 @@ public class MS2HttpProxyHandler implements MS2Proxy.Handler {
          * online mode by default.
          */
         SimpleHTTPRequest request;
-		if (action.equalsIgnoreCase("authenticate")) {
-			request = new SimpleHTTPRequest(ms2Proxy.routes.getAuthenticateURL());
-			request.addPost("username", data.username);
-			request.addPost("password", data.password);
-			request.addPost("clientToken", data.clientToken);
-			//return this.yggdrasilOffline.authenticate(data);
-		} else if (action.equalsIgnoreCase("refresh")) {
-			request = new SimpleHTTPRequest(ms2Proxy.routes.getRefreshURL());
-			request.addPost("clientToken", data.clientToken);
-			request.addPost("accessToken", data.accessToken);
-			//return this.yggdrasilOffline.refresh(data);
-		} else if (action.equalsIgnoreCase("invalidate")) {
-			request = new SimpleHTTPRequest(ms2Proxy.routes.getInvalidateURL());
-			request.addPost("clientToken", data.clientToken);
-			request.addPost("accessToken", data.accessToken);
-			//return this.yggdrasilOffline.invalidate(data);
-		} else {
-			throw new IllegalArgumentException("Unknown action " + action);
-		}
-		
-		return new String(request.doPost(Proxy.NO_PROXY), Charset.forName("utf-8"));
-	}
+        if (action.equalsIgnoreCase("authenticate")) {
+            request = new SimpleHTTPRequest(ms2Proxy.routes.getAuthenticateURL());
+            request.addPost("username", data.username);
+            request.addPost("password", data.password);
+            request.addPost("clientToken", data.clientToken);
+
+            //return this.yggdrasilOffline.authenticate(data);
+        } else if (action.equalsIgnoreCase("refresh")) {
+            request = new SimpleHTTPRequest(ms2Proxy.routes.getRefreshURL());
+            request.addPost("clientToken", data.clientToken);
+            request.addPost("accessToken", data.accessToken);
+
+            //return this.yggdrasilOffline.refresh(data);
+        } else if (action.equalsIgnoreCase("invalidate")) {
+            request = new SimpleHTTPRequest(ms2Proxy.routes.getInvalidateURL());
+            request.addPost("clientToken", data.clientToken);
+            request.addPost("accessToken", data.accessToken);
+
+            //return this.yggdrasilOffline.invalidate(data);
+        } else {
+            throw new IllegalArgumentException("Unknown action " + action);
+        }
+
+        return new String(request.doPost(Proxy.NO_PROXY), Charset.forName("utf-8"));
+    }
+
+    private String authMultiplayerAction(String action, String data, MS2Proxy ms2Proxy) {
+        UniversalLauncher.log.info("Proxy - auth - action: " + action + ", data - " + data);
+
+        SimpleHTTPRequest request;
+        if(action.equalsIgnoreCase("joinserver")) {
+            request = new SimpleHTTPRequest(ms2Proxy.routes.getJoinServerURL());
+            //return "OK";
+        } else if(action.equalsIgnoreCase("checkserver")) {
+            request = new SimpleHTTPRequest(ms2Proxy.routes.getCheckServerURL());
+            //return "YES";
+        } else {
+            throw new IllegalArgumentException("Unknown action " + action);
+        }
+
+        request.addGet(data);
+        return new String(request.doGet(Proxy.NO_PROXY), Charset.forName("utf-8"));
+    }
 }
--- src/com/mineshaftersquared/proxy/MS2Proxy.java
+++ src/com/mineshaftersquared/proxy/MS2Proxy.java
@@ -14,187 +14,209 @@ import org.apache.commons.logging.LogFactory;
 import com.mineshaftersquared.UniversalLauncher;
 
 public class MS2Proxy implements Runnable {
-	
-	public static final int MAX_NUM_CONNECTIONS = 16;
-	public static final int TIMEOUT_DURATION = 5 * 1000;
-	
-	public static Pattern SKIN_URL = Pattern.compile("http://skins\\.minecraft\\.net/MinecraftSkins/(.+?)\\.png");
-	public static Pattern CLOAK_URL = Pattern.compile("http://skins\\.minecraft\\.net/MinecraftCloaks/(.+?)\\.png");
-	public static Pattern AUTH_URL = Pattern.compile("http://authserver\\.mojang\\.com/(.*)");
-	public static Pattern CHECKSERVER_URL = Pattern.compile("http://session.minecraft.net/game/checkserver.jsp(.*)");
-	public static Pattern JOINSERVER_URL = Pattern.compile("http://session.minecraft.net/game/joinserver.jsp(.*)");
-	public static final Log log = LogFactory.getFactory().getInstance("[MS2Proxy]");
-	
-	public final RoutesDataSource routes;
-	private ServerSocket server;
-	private HandlerFactory handlerFactory;
-	private volatile boolean shouldStop;
-	private boolean hasStarted;
-	private final Object isRunningLock;
-	private boolean isInitialized;
-	private final Object isInitializedLock;
-	
-	public MS2Proxy(RoutesDataSource routes, HandlerFactory handlerFactory) {
-		this.routes = routes;
-		this.server = null;
-		this.handlerFactory = handlerFactory;
-		this.shouldStop = false;
-		this.hasStarted = false;
-		this.isRunningLock = new Object();
-		this.isInitialized = false;
-		this.isInitializedLock = new Object();
-	}
-	
-	public Thread async() {
-		try {
-			this.initialize();
-			return new Thread(this);
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
-		return null;
-	}
-	
-	public Thread startAsync() {
-		return this.startAsync(true);
-	}
-	
-	public Thread startAsync(boolean daemon) {
-		Thread t = this.async();
-		if (t != null) {
-			t.setDaemon(daemon);
-			t.start();
-		}
-		return t;
-	}
-	
-	@Override
-	public void run() {
-		synchronized (this.isRunningLock) {
-			if (this.hasStarted) {
-				throw new IllegalStateException("MS2Proxy already running");
-			}
-			this.hasStarted = true;
-		}
-		try {
-			while (true) {
-				Socket s = null;
-				try {
-					if (this.shouldStop) {
-						break;
-					}
-					s = this.server.accept();
-					final Socket socket = s;
-					new Thread(new Runnable() {
-						@Override
-						public void run() {
-							MS2Proxy.this.handlerFactory.createHandler().handle(MS2Proxy.this, socket);
-						}
-					}).start();
-				} catch (SocketTimeoutException acceptable) {
-					IOUtils.closeQuietly(s);
-				} catch (IOException ex) {
-					ex.printStackTrace();
-					IOUtils.closeQuietly(s);
-				}
-			}
-		} finally {
-			IOUtils.closeQuietly(this.server);
-		}
-		UniversalLauncher.log.info("MS2Proxy done");
-	}
-	
-	public void initialize() throws IOException {
-		synchronized (this.isInitializedLock) {
-			if (this.isInitialized) {
-				throw new IllegalStateException("MS2Proxy already initialized");
-			}
-			this.isInitialized = true;
-		}
-		this.server = new ServerSocket(0, MAX_NUM_CONNECTIONS, InetAddress.getLoopbackAddress());
-		UniversalLauncher.log.info("Proxy on: " + this.server.getLocalSocketAddress().toString());
-		this.server.setSoTimeout(TIMEOUT_DURATION);
-	}
-	
-	public int getProxyPort() {
-		return this.server.getLocalPort();
-	}
-	
-	public void stopProxy() {
-		this.shouldStop = true;
-	}
-	
-	public static interface Handler {
-		public void handle(MS2Proxy ms2Proxy, Socket socket);
-	}
-	
-	public static interface HandlerFactory {
-		public Handler createHandler();
-	}
-	
-	public static interface RoutesDataSource {
-		public String getSkinURL();
-		public String getCloakURL();
-		// authenticate.js
-		public String getAuthenticateURL();
-		public String getRefreshURL();
-		public String getInvalidateURL();
-		// game.js
-		public String getVersionURL();
-		public String getLoginURL();
-		// launcher.js
-		public String getLauncherVersionURL();
-	}
-	
-	public static class MS2RoutesDataSource implements RoutesDataSource {
-		public final String baseURL;
-		
-		/**
-		 * Default is http://api.mineshaftersquared.com
-		 * @param baseURL
-		 */
-		public MS2RoutesDataSource(String baseURL) {
-			this.baseURL = baseURL;
-		}
-		@Override
-		public String getSkinURL() {
-			return this.baseURL + "/skin";
-		}
 
-		@Override
-		public String getCloakURL() {
-			return this.baseURL + "/cloak";
-		}
+    public static final int MAX_NUM_CONNECTIONS = 16;
+    public static final int TIMEOUT_DURATION = 5 * 1000;
 
-		@Override
-		public String getAuthenticateURL() {
-			return this.baseURL + "/authenticate";
-		}
+    public static Pattern SKIN_URL = Pattern.compile("http://skins\\.minecraft\\.net/MinecraftSkins/(.+?)\\.png");
+    public static Pattern CLOAK_URL = Pattern.compile("http://skins\\.minecraft\\.net/MinecraftCloaks/(.+?)\\.png");
+    public static Pattern AUTH_URL = Pattern.compile("http://authserver\\.mojang\\.com/(.*)");
+    public static Pattern CHECKSERVER_URL = Pattern.compile("http://session.minecraft.net/game/checkserver.jsp(.*)");
+    public static Pattern JOINSERVER_URL = Pattern.compile("http://session.minecraft.net/game/joinserver.jsp(.*)");
+    public static final Log log = LogFactory.getFactory().getInstance("[MS2Proxy]");
 
-		@Override
-		public String getRefreshURL() {
-			return this.baseURL + "/refresh";
-		}
+    public final RoutesDataSource routes;
+    private ServerSocket server;
+    private HandlerFactory handlerFactory;
+    private volatile boolean shouldStop;
+    private boolean hasStarted;
+    private final Object isRunningLock;
+    private boolean isInitialized;
+    private final Object isInitializedLock;
 
-		@Override
-		public String getInvalidateURL() {
-			return this.baseURL + "/invalidate";
-		}
+    public MS2Proxy(RoutesDataSource routes, HandlerFactory handlerFactory) {
+        this.routes = routes;
+        this.server = null;
+        this.handlerFactory = handlerFactory;
+        this.shouldStop = false;
+        this.hasStarted = false;
+        this.isRunningLock = new Object();
+        this.isInitialized = false;
+        this.isInitializedLock = new Object();
+    }
 
-		@Override
-		public String getVersionURL() {
-			return this.baseURL + "/version";
-		}
+    public Thread async() {
+        try {
+            this.initialize();
+            return new Thread(this);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
 
-		@Override
-		public String getLoginURL() {
-			return this.baseURL + "/login";
-		}
+    public Thread startAsync() {
+        return this.startAsync(true);
+    }
 
-		@Override
-		public String getLauncherVersionURL() {
-			return this.baseURL + "/launcher/version";
-		}
-		
-	}
+    public Thread startAsync(boolean daemon) {
+        Thread t = this.async();
+        if (t != null) {
+            t.setDaemon(daemon);
+            t.start();
+        }
+        return t;
+    }
+
+    @Override
+    public void run() {
+        synchronized (this.isRunningLock) {
+            if (this.hasStarted) {
+                throw new IllegalStateException("MS2Proxy already running");
+            }
+            this.hasStarted = true;
+        }
+        try {
+            while (true) {
+                Socket s = null;
+                try {
+                    if (this.shouldStop) {
+                        break;
+                    }
+                    s = this.server.accept();
+                    final Socket socket = s;
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            MS2Proxy.this.handlerFactory.createHandler().handle(MS2Proxy.this, socket);
+                        }
+                    }).start();
+                } catch (SocketTimeoutException acceptable) {
+                    IOUtils.closeQuietly(s);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                    IOUtils.closeQuietly(s);
+                }
+            }
+        } finally {
+            IOUtils.closeQuietly(this.server);
+        }
+        UniversalLauncher.log.info("MS2Proxy done");
+    }
+
+    public void initialize() throws IOException {
+        synchronized (this.isInitializedLock) {
+            if (this.isInitialized) {
+                throw new IllegalStateException("MS2Proxy already initialized");
+            }
+            this.isInitialized = true;
+        }
+        this.server = new ServerSocket(0, MAX_NUM_CONNECTIONS, InetAddress.getLoopbackAddress());
+        UniversalLauncher.log.info("Proxy on: " + this.server.getLocalSocketAddress().toString());
+        this.server.setSoTimeout(TIMEOUT_DURATION);
+    }
+
+    public int getProxyPort() {
+        return this.server.getLocalPort();
+    }
+
+    public void stopProxy() {
+        this.shouldStop = true;
+    }
+
+    public static interface Handler {
+        public void handle(MS2Proxy ms2Proxy, Socket socket);
+    }
+
+    public static interface HandlerFactory {
+        public Handler createHandler();
+    }
+
+    public static interface RoutesDataSource {
+        public String getSkinURL();
+
+        public String getCloakURL();
+
+        // authenticate.js
+        public String getAuthenticateURL();
+
+        public String getRefreshURL();
+
+        public String getInvalidateURL();
+
+        // game.js
+        public String getVersionURL();
+
+        public String getLoginURL();
+
+        public String getJoinServerURL();
+
+        public String getCheckServerURL();
+
+        // launcher.js
+        public String getLauncherVersionURL();
+    }
+
+    public static class MS2RoutesDataSource implements RoutesDataSource {
+        public final String baseURL;
+
+        /**
+         * Default is http://api.mineshaftersquared.com
+         *
+         * @param baseURL
+         */
+        public MS2RoutesDataSource(String baseURL) {
+            this.baseURL = baseURL;
+        }
+
+        @Override
+        public String getSkinURL() {
+            return this.baseURL + "/skin";
+        }
+
+        @Override
+        public String getCloakURL() {
+            return this.baseURL + "/cloak";
+        }
+
+        @Override
+        public String getAuthenticateURL() {
+            return this.baseURL + "/authenticate";
+        }
+
+        @Override
+        public String getRefreshURL() {
+            return this.baseURL + "/refresh";
+        }
+
+        @Override
+        public String getInvalidateURL() {
+            return this.baseURL + "/invalidate";
+        }
+
+        @Override
+        public String getVersionURL() {
+            return this.baseURL + "/version";
+        }
+
+        @Override
+        public String getLoginURL() {
+            return this.baseURL + "/login";
+        }
+
+        @Override
+        public String getJoinServerURL() {
+            return this.baseURL + "/game/joinserver";
+        }
+
+        @Override
+        public String getCheckServerURL() {
+            return this.baseURL + "/game/checkserver";
+        }
+
+        @Override
+        public String getLauncherVersionURL() {
+            return this.baseURL + "/launcher/version";
+        }
+    }
 }
